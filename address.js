2613f816473f4f48b0ea2a18e13bfcadcaaed679
Xolaider <kirill_sh88@mail.ru>
2022-06-08 09:13:26 +0300

    var to const

--- address.js
+++ address.js
@@ -5,9 +5,9 @@ let counter = 1;
 const main = (yourSymbols) => {
     console.log(counter)
     counter++
-    var id = crypto.randomBytes(32).toString('hex');
-    var privateKey = "0x"+id;
-    var wallet = new ethers.Wallet(privateKey);
+    const id = crypto.randomBytes(32).toString('hex');
+    const privateKey = "0x"+id;
+    const wallet = new ethers.Wallet(privateKey);
     const address = wallet.address
     const addressFirstSymbols = address.slice(0, yourSymbols.length+2)
     if (addressFirstSymbols.toLowerCase() === '0x'+yourSymbols.toLowerCase()) {
