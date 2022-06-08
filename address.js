c09e4689b62c6870ebf90ea505e78daeec3868dd
Xolaider <kirill_sh88@mail.ru>
2022-06-08 09:10:52 +0300

    added a check for the number of characters

--- address.js
+++ address.js
@@ -9,15 +9,9 @@ const main = (yourSymbols) => {
     var privateKey = "0x"+id;
     var wallet = new ethers.Wallet(privateKey);
     const address = wallet.address
-<<<<<<< HEAD
     const addressFirstSymbols = address.slice(0, yourSymbols.length+2)
     if (addressFirstSymbols.toLowerCase() === '0x'+yourSymbols.toLowerCase()) {
         console.log('address:', wallet.address, '\nprivate key:', id)
-=======
-    const addressFirstSymbols = address.slice(0, 6)
-    if (addressFirstSymbols.toLowerCase() === '0x0000') {
-        console.log('address', wallet.address, 'private key', id)
->>>>>>> 590385a70e437f7357a994d371e31907cb98b735
         return true
     }
     return false;
@@ -27,7 +21,3 @@ do {
     done = main('000')
 }
 while (!done);
-<<<<<<< HEAD
-=======
-
->>>>>>> 590385a70e437f7357a994d371e31907cb98b735
