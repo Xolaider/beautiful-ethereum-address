20d26d27818b5299602c3a3b15d3d4b25f524731
Xolaider <kirill_sh88@mail.ru>
2022-06-08 09:27:59 +0300

    add timer

--- address.js
+++ address.js
@@ -2,6 +2,8 @@ var ethers = require('ethers');
 var crypto = require('crypto');
 let done = false;
 let counter = 1;
+let now = new Date()
+
 const main = (yourSymbols) => {
     console.log(counter)
     counter++
@@ -11,13 +13,14 @@ const main = (yourSymbols) => {
     const address = wallet.address
     const addressFirstSymbols = address.slice(0, yourSymbols.length+2)
     if (addressFirstSymbols.toLowerCase() === '0x'+yourSymbols.toLowerCase()) {
-        console.log('address:', wallet.address, '\nprivate key:', id)
+        console.log('address:', wallet.address, '\nprivate key:', id, '\nsearching time:', (new Date-now)/1000, 'seconds')
         return true
     }
     return false;
 }
 
 do {
-    done = main('000')
+    done = main('00000')
 }
 while (!done);
+console.log
